ecad0fcf2ee96cadfc238ffa079850b9ad630363
JustLivingg <JustinTLivingston@gmail.com>
2022-03-15 13:36:36 -0700

    Two way binding.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,20 +49,26 @@ const ExpenseForm = () => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      data: new Date(enteredData)
+      data: new Date(enteredData),
     };
 
-  console.log(expenseData)
+    console.log(expenseData);
+    // Two way binding to clear forms after submit.
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredData("");
   };
-
-  
 
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -70,6 +76,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -79,6 +86,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredData}
             onChange={dataChangeHandler}
           />
         </div>
