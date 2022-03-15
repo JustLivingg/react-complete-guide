500b192d767d7222ee32e81df31a84a4c8c5a00c
JustLivingg <JustinTLivingston@gmail.com>
2022-03-15 11:26:16 -0700

    Working with State.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,31 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import React, { useState } from "react";
+
+// useState is a function provided by React Library, used for State.
+// useState returns an array.
+// Hooks start with the word use.
 
 //importing ExpenseDate and using it on line 11.
 //Nesting components in components for organization.
 //Too much nesting can get confusing. Really need to think out working tree.
 
 const ExpenseItem = (props) => {
+  // Must happen inside function.
+  const [title, setTitle] = useState(props.title);
   
   // Naming conventions end with Handler. means its being called and attached by an event.
   const clickHandler = () => {
-    console.log("CLicked!!!!");
+    setTitle('Updated!');
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate data={props.data} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+// Where the page is intialized, calling the App.js file to load what is displayed.
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
