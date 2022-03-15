443f5678772258953d5159ef4546b43eaca986d2
JustLivingg <JustinTLivingston@gmail.com>
2022-03-15 14:38:18 -0700

    Working with Events and State.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,22 +13,21 @@ import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
   // Must happen inside function.
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // Naming conventions end with Handler. means its being called and attached by an event.
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate data={props.data} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
