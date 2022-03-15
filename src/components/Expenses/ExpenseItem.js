058721474da0d1bc5f58d27fdf36885d5ac4cfb2
JustLivingg <JustinTLivingston@gmail.com>
2022-03-15 11:05:13 -0700

    Listening to Events & working with Event Handlers.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,18 +2,27 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
+//importing ExpenseDate and using it on line 11.
+//Nesting components in components for organization.
+//Too much nesting can get confusing. Really need to think out working tree.
 
 const ExpenseItem = (props) => {
+  
+  // Naming conventions end with Handler. means its being called and attached by an event.
+  const clickHandler = () => {
+    console.log("CLicked!!!!");
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate data={props.data}/>
+      <ExpenseDate data={props.data} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,3 +1,5 @@
+//UI Components can also be made if used in multiple places with same CSS.
+
 const Card = (props) => {
     const classes = 'card ' + props.className;
     return <div className ={classes}>{props.children}</div>
